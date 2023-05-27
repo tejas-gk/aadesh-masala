6c674db0acce9197440ded648f2f1f05446998dc
tejas-gk <tejgankri@outlook.com>
2023-05-28 05:02:50 +0530

    fix:produt size

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -13,16 +13,16 @@ export default function ProductFilter() {
 
     return (
         <div className="mt-8">
-            <div className="flex gap-4 justify-center items-center">
+            <div className="flex gap-4 justify-center items-center bg-gray-200 w-fit mx-auto">
                 <TagButton tag="all" selectedTag={selectedTag} onClick={handleTagChange} />
                 <TagButton tag="blended" selectedTag={selectedTag} onClick={handleTagChange} />
                 <TagButton tag="grounded" selectedTag={selectedTag} onClick={handleTagChange} />
                 <TagButton tag="other" selectedTag={selectedTag} onClick={handleTagChange} />
             </div>
-            <div className="grid grid-cols-8 gap-4 mt-4">
+            <div className="grid lg:grid-cols-8 sm:grid-cols-4 grid-cols-3 gap-4 mt-4">
                 {filteredProducts.map((product, index) => (
                     <div key={index}>
-                        <img src={product.image} alt={`Product ${index}`} className="h-24 w-24" />
+                        <img src={product.image} alt={`Product ${index}`} className="h-42 w-42" />
                     </div>
                 ))}
             </div>
--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -1,4 +1,5 @@
-import { images } from '../../assets/images'
+import { images } from '../../data/images'
+
 const navItems = [
     {
         name: 'Home',
@@ -19,28 +20,30 @@ const navItems = [
         name: "Contact"
     }
 ]
+
 export default function Navbar() {
     return (
-        <div className="flex items-center  bg-white">
-            <img
-                src={images.logo}
-                alt='logo'
-                className='h-24 w-24 ml-8'
-            />
-            <div className="ml-auto mr-8 hidden mr-8 lg:block max-h-24">
-                {
-                    navItems.map((item, index) => {
-                        return (
+        <div className="relative bg-white">
+            <div className="flex items-center group">
+                <img
+                    src={images.logo}
+                    alt='logo'
+                    className='h-24 w-24 ml-8'
+                />
+                <div className="ml-auto hidden mr-8 lg:block max-h-24">
+                    {
+                        navItems.map((item, index) => (
                             <div key={index} className='inline-block mx-2'>
                                 <a href='/' className='txt relative text-[19px] font-bold cursor-pointer'>
                                     {item.name}
                                     <span className="underline-a bg-black"></span>
                                 </a>
                             </div>
-                        )
-                    })
-                }
+                        ))
+                    }
+                </div>
             </div>
+            <div className="absolute top-0 left-0 w-full h-0 bg-black transition-height duration-300 ease-in-out group-hover:h-4"></div>
         </div>
     )
 }
