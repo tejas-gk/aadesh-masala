91a74919f177f0bd0272435413fe98f1207604e1
tejas-gk <tejgankri@outlook.com>
2023-05-28 08:11:40 +0530

    feat:prev and next reci[ie

--- src/pages/Recipies.jsx
+++ src/pages/Recipies.jsx
@@ -1,7 +1,9 @@
 import { useParams } from 'react-router-dom';
 import { recipies } from '../data/recipie';
+import Card from '../components/common/Card';
 export default function Recipies() {
     const { id } = useParams();
+    console.log(recipies[id], parseInt(id) + 1)
     return (
         <div className='mt-28'>
             <img src={recipies[id].image} alt={recipies[id].title} className="w-full h-96 rounded-lg mx-5  mr-16 object-cover" />
@@ -21,6 +23,49 @@ export default function Recipies() {
                             {ingredient}
                         </div>
                     ))
+
+                }
+            </div>
+
+
+            <div className='flex justify-between mt-9'>
+
+                {
+                    recipies[id - 1] != null ?
+                        <div className='
+                    mx-5 
+                    '>
+                            <a href={`/recipies/${id - 1}`} className='flex items-center
+'>
+                            <img src={recipies[id - 1].image} alt={recipies[id - 1].title} className="w-16 h-16 rounded-full mx-5  object-cover" />
+                            <div>
+                                <h2 className='text-gray-400 text-lg font-semibold'>Previous Recipie</h2>
+                                <h3 className='font-bold first-letter:uppercase'>
+                                    {recipies[id - 1].title}
+                                </h3>
+                                </div>
+                            </a>
+                        </div>
+                        : null
+                }
+
+                {
+                    recipies[parseInt(id) + 1] != null ?
+                        <div className='
+                mx-5
+                '>
+                            <a href={`/recipies/${parseInt(id) + 1}`} className='flex items-center flex-row-reverse'>
+                                
+                            <img src={recipies[parseInt(id) + 1].image} alt={recipies[parseInt(id) + 1].title} className="w-16 h-16 rounded-full mx-5  object-cover" />
+                            <div>
+                                <h2 className='text-gray-400 text-lg font-semibold'>Next Recipie</h2>
+                                <h3 className='font-bold first-letter:uppercase'>
+                                    {recipies[parseInt(id) + 1].title}
+                                </h3>
+                            </div>
+                            </a>
+                        </div>
+                        : null
 
                 }
             </div>
