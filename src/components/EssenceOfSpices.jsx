68cae11e361d08eb8cfa58bcef5ce6c3bdad387d
tejas-gk <tejgankri@outlook.com>
2023-05-28 17:34:33 +0530

    fix:view more arrow

--- src/components/EssenceOfSpices.jsx
+++ src/components/EssenceOfSpices.jsx
@@ -1,9 +1,8 @@
-import { images } from "../data/images";
-
+import { ArrowRight } from "react-feather";
 export default function EssenceOfSpices() {
   return (
     <div className="essence">
-      <div className="text-center lg:mx-28 mx-5">
+      <div className="text-center lg:mx-16 mx-5">
         <div className="relative w-fit text-center mx-auto mb-9 mt-4 z-20">
           <div className='txt cursor-pointer mx-auto lg:text-5xl text-3xl  text-red-500 font-bold'>
             The Essence of Indian Spices
@@ -13,15 +12,18 @@ export default function EssenceOfSpices() {
         <p className="lg:mx-[22rem]">
           Welcome to Aadesh Masala, an Indian spices brand that is dedicated to bringing the flavors of traditional Indian cuisine to every household in India. Our mission is to provide the best quality spices to our customers, ensuring that every meal is full of flavor and packed with nutrition. Over the years, we have grown from a small local business to a national brand, serving customers across India. We take pride in our commitment to quality, sourcing the freshest and finest ingredients to make our spices. Our products are free from any harmful additives, ensuring that our customers can enjoy healthy and delicious meals.
         </p>
-        <div className="txt flex items-center justify-center relative mt-5  w-fit mx-auto">
-          <div className="fill h-10 w-12 bg-black rounded-full text-white flex items-center justify-center ">
-            <img src={images.rightArrow} alt="rightArrow" className="svg-white" />
+
+        <div className="txt flex items-center justify-center relative mt-5 w-fit mx-auto">
+          <div className="fill h-10 w-12 bg-black rounded-full text-white flex items-center justify-center">
+            <ArrowRight className="right-arrow h-6 w-6" />
           </div>
           <p className="view-more ml-10 z-50">
             View more
           </p>
-          <div className="h-1 w-full bg-black transform scale-x-0 transition-transform duration-300 origin-left absolute bottom-0 left-0"></div>
+          {/* <div className="h-1 w-full bg-black transform scale-x-0 transition-transform duration-300 origin-left absolute bottom-0 left-0"></div> */}
         </div>
+
+
       </div>
       <div className="image-container">
         <div className="top-left-image"></div>
