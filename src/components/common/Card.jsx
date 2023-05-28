018e28f69bbc43d2832164d5ae546b4bb44027d4
tejas-gk <tejgankri@outlook.com>
2023-05-28 06:28:29 +0530

    fix:card

--- src/components/common/Card.jsx
+++ src/components/common/Card.jsx
@@ -3,12 +3,20 @@ import { Link } from 'react-router-dom';
 
 export default function Card({ data }) {
     return (
-        <div>
+        <div className=''>
             <Link to={`${data.link}`}>
-                <img src={data.image} alt={data.title} className="w-[300px] h-[300px] rounded-lg" />
+                <div
+                    className='w-[300px] h-[300px] overflow-hidden relative rounded-lg'
+                >
+                    <img
+                        src={data.image}
+                        alt={data.title}
+                        className="hover:scale-105 w-full h-full object-cover transition-all duration-300 ease-in-out"
+                    />
+                </div>
 
                 <p className='text-red-500 first-letter:uppercase font-semibold text-lg'>
-                {data.title}
+                    {data.title}
                 </p>
             </Link>
         </div>
